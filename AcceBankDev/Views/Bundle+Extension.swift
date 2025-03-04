0117d566793c764ad975a56280a95e3796debeec
Khushi6311 <khushi@miraclecloud-technology.com>
2025-03-04 18:08:18 +0530

    Changes: Changed Move Money Screen design. Added Logo on login,overview page. Added languages french and english. Did changes in all files as needed.

---
+++ AcceBankDev/Views/Bundle+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Bundle+Extension.swift
+//  AcceBankDev
+//
+//  Created by MCT on 03/03/25.
+//
+
+import SwiftUI
+
+struct Bundle_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Bundle_Extension()
+}
